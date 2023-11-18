4fcbfb192387b6f6db1407777d9398dfccf3f544
Rustam Ibragimov <rustamibragimov67@yandex.ru>
2023-11-18 12:45:02 +0500

    Добавлены материалы семинара №1

---
+++ seminar1/src/lection/MainTypesOfErrors.java
@@ -0,0 +1,29 @@
+package lection;
+
+public class MainTypesOfErrors {
+
+//     Типы ошибок. Синтаксические (отсутствие точки с запятой)
+//    public static void main(String[] args) {
+//        System.out.println("Hello world!")
+//    }
+
+//     Типы ошибок. Логическая (должно быть строгое сравнение)
+//    public static void main(String[] args) {
+//        compareNumbers(2, 2); // Вызывается метод сравнения двух чисел
+//    }
+//    private static void compareNumbers(int a, int b) {
+//        if (a > b) {
+//            System.out.printf("%d больше, чем %d", a, b);
+//        }
+//        if (a <= b) {  // Допущена ошибка -знак <= вместо <
+//            System.out.printf("%d меньше, чем %d", a, b);
+//        }
+//    }
+//
+////     Типы ошибок. Ошибка выполнения (деление на 0)
+//  public static void main(String[] args) {
+//      int a = 10, b = 0;
+//      System.out.printf("Result: %d", a / b);
+//  }
+
+}
