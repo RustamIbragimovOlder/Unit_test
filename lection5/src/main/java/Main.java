94756ecee7aac30479b656fb9939ab805ef02dba
Rustam Ibragimov <rustamibragimov67@yandex.ru>
2023-11-26 12:01:40 +0500

    Добавлены материалы лекции №5

---
+++ lection5/src/main/java/Main.java
@@ -0,0 +1,20 @@
+package main.java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver",
+                "C:/chromedriver_win32/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com/");
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("GeekBrains");
+        searchBox.submit();
+        Thread.sleep(10000);
+        driver.quit();
+    }
+}
